654123503cf96384975360e68b6981899fa12961
Louis <louis@spoonradio.co>
2022-05-26 17:19:09 +0900

    UITableView added

---
+++ Sources/CocoaChain/UIKit/UITableView + Chain.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+public extension Chain where T: UITableView {
+    
+    @discardableResult
+    func delegate(_ delegate: UITableViewDelegate?) -> Self {
+        base.delegate = delegate
+        return self
+    }
+    
+    @discardableResult
+    func dataSource(_ dataSource: UITableViewDataSource?) -> Self {
+        base.dataSource = dataSource
+        return self
+    }
+    
+    @discardableResult
+    func reloadData() -> Self {
+        base.reloadData()
+        return self
+    }
+}
