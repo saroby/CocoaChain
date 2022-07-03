2f5b56f9078671795ab560510725cec0f854151a
Louis <louis@spoonradio.co>
2022-07-04 01:35:11 +0900

    UIView updated

--- Sources/CocoaChain/UIKit/UIView + Chain.swift
+++ Sources/CocoaChain/UIKit/UIView + Chain.swift
@@ -4,14 +4,293 @@ import SnapKit
 public extension Chain where T: UIView {
     
     @discardableResult
-    func isHidden(_ isHidden: Bool) -> Self {
-        base.isHidden = isHidden
+    func isUserInteractionEnabled(_ isUserInteractionEnabled: Bool) -> Self {
+        base.isUserInteractionEnabled = isUserInteractionEnabled
         return self
     }
     
     @discardableResult
     func tag(_ tag: Int) -> Self {
         base.tag = tag
+        return self
+    }
+    
+    @discardableResult
+    func layer(_ closure: (_ layer: CALayer) -> Void) -> Self {
+        closure(base.layer)
+        return self
+    }
+    
+    @available(iOS 14.0, *)
+    @discardableResult
+    func focusGroupIdentifier(_ focusGroupIdentifier: String?) -> Self {
+        base.focusGroupIdentifier = focusGroupIdentifier
+        return self
+    }
+    
+    @available(iOS 15.0, *)
+    @discardableResult
+    func focusGroupPriority(_ focusGroupPriority: UIFocusGroupPriority) -> Self {
+        base.focusGroupPriority = focusGroupPriority
+        return self
+    }
+    
+    @available(iOS 15.0, *)
+    func focusEffect(_ focusEffect: UIFocusEffect?) -> Self {
+        base.focusEffect = focusEffect
+        return self
+    }
+    
+    @available(iOS 15.0, *)
+    @discardableResult
+    func semanticContentAttribute(_ semanticContentAttribute: UISemanticContentAttribute) -> Self {
+        base.semanticContentAttribute = semanticContentAttribute
+        return self
+    }
+}
+
+public extension Chain where T: UIView {
+    
+    @discardableResult
+    func size(_ size: CGSize) -> Self {
+        base.frame.size = size
+        return self
+    }
+    
+    @discardableResult
+    func frame(_ frame: CGRect) -> Self {
+        base.frame = frame
+        return self
+    }
+    
+    @discardableResult
+    func bounds(_ bounds: CGRect) -> Self {
+        base.bounds = bounds
+        return self
+    }
+    
+    @discardableResult
+    func center(_ center: CGPoint) -> Self {
+        base.center = center
+        return self
+    }
+    
+    @discardableResult
+    func transform(_ transform: CGAffineTransform) -> Self {
+        base.transform = transform
+        return self
+    }
+    
+    @available(iOS 13.0, *)
+    @discardableResult
+    func transform3D(_ transform3D: CATransform3D) -> Self {
+        base.transform3D = transform3D
+        return self
+    }
+    
+    @discardableResult
+    func contentScaleFactor(_ contentScaleFactor: CGFloat) -> Self {
+        base.contentScaleFactor = contentScaleFactor
+        return self
+    }
+    
+    @discardableResult
+    func isMultipleTouchEnabled(_ isMultipleTouchEnabled: Bool) -> Self {
+        base.isMultipleTouchEnabled = isMultipleTouchEnabled
+        return self
+    }
+    
+    @discardableResult
+    func isExclusiveTouch(_ isExclusiveTouch: Bool) -> Self {
+        base.isExclusiveTouch = isExclusiveTouch
+        return self
+    }
+    
+    @discardableResult
+    func autoresizesSubviews(_ autoresizesSubviews: Bool) -> Self {
+        base.autoresizesSubviews = autoresizesSubviews
+        return self
+    }
+    
+    @discardableResult
+    func autoresizingMask(_ autoresizingMask: UIView.AutoresizingMask) -> Self {
+        base.autoresizingMask = autoresizingMask
+        return self
+    }
+    
+    @discardableResult
+    func sizeToFit() -> Self {
+        base.sizeToFit()
+        return self
+    }
+    
+}
+
+public extension Chain where T: UIView {
+    
+    @discardableResult
+    func removeFromSuperview() -> Self {
+        base.removeFromSuperview()
+        return self
+    }
+    
+    @discardableResult
+    func insertSubview(_ subview: UIView, at: Int) -> Self {
+        base.insertSubview(subview, at: at)
+        return self
+    }
+    
+    @discardableResult
+    func exchangeSubview(at index1: Int, withSubviewAt index2: Int) -> Self {
+        base.exchangeSubview(at: index1, withSubviewAt: index2)
+        return self
+    }
+    
+    @discardableResult
+    func addSubview(_ view: UIView) -> Self {
+        base.addSubview(view)
+        return self
+    }
+    
+    @discardableResult
+    func insertSubview(_ view: UIView, belowSubview siblingSubview: UIView) -> Self {
+        base.insertSubview(view, belowSubview: siblingSubview)
+        return self
+    }
+    
+    @discardableResult
+    func insertSubview(_ view: UIView, aboveSubview siblingSubview: UIView) -> Self {
+        base.insertSubview(view, aboveSubview: siblingSubview)
+        return self
+    }
+    
+    @discardableResult
+    func bringSubviewToFront(_ view: UIView) -> Self {
+        base.bringSubviewToFront(view)
+        return self
+    }
+    
+    @discardableResult
+    func sendSubviewToBack(_ view: UIView) -> Self {
+        base.sendSubviewToBack(view)
+        return self
+    }
+    
+    @discardableResult
+    func didAddSubview(_ subview: UIView) -> Self {
+        base.didAddSubview(subview)
+        return self
+    }
+    
+    @discardableResult
+    func willRemoveSubview(_ subview: UIView) -> Self {
+        base.willRemoveSubview(subview)
+        return self
+    }
+    
+    @discardableResult
+    func willMove(toSuperview newSuperview: UIView?) -> Self {
+        base.willMove(toSuperview: newSuperview)
+        return self
+    }
+    
+    @discardableResult
+    func didMoveToSuperview() -> Self {
+        base.didMoveToSuperview()
+        return self
+    }
+    
+    @discardableResult
+    func willMove(toWindow newWindow: UIWindow?) -> Self {
+        base.willMove(toWindow: newWindow)
+        return self
+    }
+    
+    @discardableResult
+    func didMoveToWindow() -> Self {
+        base.didMoveToWindow()
+        return self
+    }
+    
+    @discardableResult
+    func setNeedsLayout() -> Self {
+        base.setNeedsLayout()
+        return self
+    }
+    
+    @discardableResult
+    func layoutIfNeeded() -> Self {
+        base.layoutIfNeeded()
+        return self
+    }
+    
+    @discardableResult
+    func layoutSubviews() -> Self {
+        base.layoutSubviews()
+        return self
+    }
+    
+    @discardableResult
+    func layoutMargins(_ layoutMargins: UIEdgeInsets) -> Self {
+        base.layoutMargins = layoutMargins
+        return self
+    }
+    
+    @discardableResult
+    func directionalLayoutMargins(_ directionalLayoutMargins: NSDirectionalEdgeInsets) -> Self {
+        base.directionalLayoutMargins = directionalLayoutMargins
+        return self
+    }
+    
+    @discardableResult
+    func preservesSuperviewLayoutMargins(_ preservesSuperviewLayoutMargins: Bool) -> Self {
+        base.preservesSuperviewLayoutMargins = preservesSuperviewLayoutMargins
+        return self
+    }
+    
+    @discardableResult
+    func insetsLayoutMarginsFromSafeArea(_ insetsLayoutMarginsFromSafeArea: Bool) -> Self {
+        base.insetsLayoutMarginsFromSafeArea = insetsLayoutMarginsFromSafeArea
+        return self
+    }
+    
+    @discardableResult
+    func layoutMarginsDidChange() -> Self {
+        base.layoutMarginsDidChange()
+        return self
+    }
+    
+    @discardableResult
+    func safeAreaInsetsDidChange() -> Self {
+        base.safeAreaInsetsDidChange()
+        return self
+    }
+    
+}
+
+public extension Chain where T: UIView {
+    
+    @discardableResult
+    func draw(_ rect: CGRect) -> Self {
+        base.draw(rect)
+        return self
+    }
+    
+    @discardableResult
+    func setNeedsDisplay() -> Self {
+        base.setNeedsDisplay()
+        return self
+    }
+    
+    @discardableResult
+    func setNeedsDisplay(_ rect: CGRect) -> Self {
+        base.setNeedsDisplay(rect)
+        return self
+    }
+    
+    @discardableResult
+    func clipsToBounds(_ clipsToBounds: Bool) -> Self {
+        base.clipsToBounds = clipsToBounds
         return self
     }
     
@@ -34,8 +313,14 @@ public extension Chain where T: UIView {
     }
     
     @discardableResult
-    func center(_ center: CGPoint) -> Self {
-        base.center = center
+    func clearsContextBeforeDrawing(_ clearsContextBeforeDrawing: Bool) -> Self {
+        base.clearsContextBeforeDrawing = clearsContextBeforeDrawing
+        return self
+    }
+    
+    @discardableResult
+    func isHidden(_ isHidden: Bool) -> Self {
+        base.isHidden = isHidden
         return self
     }
     
@@ -46,46 +331,212 @@ public extension Chain where T: UIView {
     }
     
     @discardableResult
-    func tintColor(_ tintColor: UIColor) -> Self {
-        base.tintColor = tintColor
-        return self
-    }
-    
-    @discardableResult
-    func frame(_ frame: CGRect) -> Self {
-        base.frame = frame
-        return self
-    }
-    
-    @discardableResult
-    func size(_ size: CGSize) -> Self {
-        base.frame.size = size
-        return self
-    }
-    
-    @discardableResult
-    func bounds(_ bounds: CGRect) -> Self {
-        base.bounds = bounds
-        return self
-    }
-    
-    @discardableResult
     func mask(_ mask: UIView?) -> Self {
         base.mask = mask
         return self
     }
     
     @discardableResult
-    func addSubview(_ subview: UIView) -> Self {
-        base.addSubview(subview)
+    func tintColor(_ tintColor: UIColor) -> Self {
+        base.tintColor = tintColor
         return self
     }
     
     @discardableResult
-    func insertSubview(_ subview: UIView, at: Int) -> Self {
-        base.insertSubview(subview, at: at)
+    func tintAdjustmentMode(_ tintAdjustmentMode: UIView.TintAdjustmentMode) -> Self {
+        base.tintAdjustmentMode = tintAdjustmentMode
         return self
     }
+    
+    @discardableResult
+    func tintColorDidChange() -> Self {
+        base.tintColorDidChange()
+        return self
+    }
+    
+}
+
+public extension Chain where T: UIView {
+    
+    @discardableResult
+    func gestureRecognizers(_ gestureRecognizers: [UIGestureRecognizer]?) -> Self {
+        base.gestureRecognizers = gestureRecognizers
+        return self
+    }
+    
+    @discardableResult
+    func addGestureRecognizer(_ gestureRecognizer: UIGestureRecognizer) -> Self {
+        base.addGestureRecognizer(gestureRecognizer)
+        return self
+    }
+    
+    @discardableResult
+    func removeGestureRecognizer(_ gestureRecognizer: UIGestureRecognizer) -> Self {
+        base.removeGestureRecognizer(gestureRecognizer)
+        return self
+    }
+    
+}
+
+public extension Chain where T: UIView {
+    
+    @discardableResult
+    func addMotionEffect(_ effect: UIMotionEffect) -> Self {
+        base.addMotionEffect(effect)
+        return self
+    }
+    
+    @discardableResult
+    func removeMotionEffect(_ effect: UIMotionEffect) -> Self {
+        base.removeMotionEffect(effect)
+        return self
+    }
+    
+    @discardableResult
+    func motionEffects(_ motionEffects: [UIMotionEffect]) -> Self {
+        base.motionEffects = motionEffects
+        return self
+    }
+    
+}
+
+public extension Chain where T: UIView {
+    
+    @discardableResult
+    func addConstraint(_ constraint: NSLayoutConstraint) -> Self {
+        base.addConstraint(constraint)
+        return self
+    }
+    
+    @discardableResult
+    func addConstraints(_ constraints: [NSLayoutConstraint]) -> Self {
+        base.addConstraints(constraints)
+        return self
+    }
+    
+    @discardableResult
+    func removeConstraint(_ constraint: NSLayoutConstraint) -> Self {
+        base.removeConstraint(constraint)
+        return self
+    }
+    
+    @discardableResult
+    func removeConstraints(_ constraints: [NSLayoutConstraint]) -> Self {
+        base.removeConstraints(constraints)
+        return self
+    }
+    
+}
+
+public extension Chain where T: UIView {
+    
+    @discardableResult
+    func updateConstraintsIfNeeded() -> Self {
+        base.updateConstraintsIfNeeded()
+        return self
+    }
+    
+    @discardableResult
+    func updateConstraints() -> Self {
+        base.updateConstraints()
+        return self
+    }
+    
+    @discardableResult
+    func setNeedsUpdateConstraints() -> Self {
+        base.setNeedsUpdateConstraints()
+        return self
+    }
+    
+}
+
+public extension Chain where T: UIView {
+    
+    @discardableResult
+    func translatesAutoresizingMaskIntoConstraints(_ translatesAutoresizingMaskIntoConstraints: Bool) -> Self {
+        base.translatesAutoresizingMaskIntoConstraints = translatesAutoresizingMaskIntoConstraints
+        return self
+    }
+    
+}
+
+public extension Chain where T: UIView {
+    
+    @discardableResult
+    func invalidateIntrinsicContentSize() -> Self {
+        base.invalidateIntrinsicContentSize()
+        return self
+    }
+    
+    @discardableResult
+    func setContentHuggingPriority(_ priority: UILayoutPriority, for axis: NSLayoutConstraint.Axis) -> Self {
+        base.setContentHuggingPriority(priority, for: axis)
+        return self
+    }
+    
+    @discardableResult
+    func setContentCompressionResistancePriority(_ priority: UILayoutPriority, for axis: NSLayoutConstraint.Axis) -> Self {
+        base.setContentCompressionResistancePriority(priority, for: axis)
+        return self
+    }
+    
+}
+
+public extension Chain where T: UIView {
+    
+    @discardableResult
+    func addLayoutGuide(_ layoutGuide: UILayoutGuide) -> Self {
+        base.addLayoutGuide(layoutGuide)
+        return self
+    }
+    
+    @discardableResult
+    func removeLayoutGuide(_ layoutGuide: UILayoutGuide) -> Self {
+        base.removeLayoutGuide(layoutGuide)
+        return self
+    }
+    
+}
+
+public extension Chain where T: UIView {
+    
+    @discardableResult
+    func exerciseAmbiguityInLayout() -> Self {
+        base.exerciseAmbiguityInLayout()
+        return self
+    }
+    
+}
+
+public extension Chain where T: UIView {
+    
+    @available(iOS 13.0, *)
+    @discardableResult
+    func overrideUserInterfaceStyle(_ overrideUserInterfaceStyle: UIUserInterfaceStyle) -> Self {
+        base.overrideUserInterfaceStyle = overrideUserInterfaceStyle
+        return self
+    }
+    
+}
+
+public extension Chain where T: UIView {
+    
+    @discardableResult
+    func minimumContentSizeCategory(minimumContentSizeCategory: UIContentSizeCategory?) -> Self {
+        base.minimumContentSizeCategory = minimumContentSizeCategory
+        return self
+    }
+    
+    @discardableResult
+    func maximumContentSizeCategory(maximumContentSizeCategory: UIContentSizeCategory?) -> Self {
+        base.maximumContentSizeCategory = maximumContentSizeCategory
+        return self
+    }
+}
+
+// (beta) Superview
+
+public extension Chain where T: UIView {
     
     @discardableResult
     func addSuperview(_ superview: UIView) -> Self {
@@ -112,54 +563,6 @@ public extension Chain where T: UIView {
     func insertSuperview(_ superview: UIView, aboveSubview: UIView) -> Self {
         base.removeFromSuperview()
         superview.insertSubview(base, aboveSubview: aboveSubview)
-        return self
-    }
-    
-    @discardableResult
-    func removeFromSuperview() -> Self {
-        base.removeFromSuperview()
-        return self
-    }
-    
-    @discardableResult
-    func bringSubviewToFront(_ view: UIView) -> Self {
-        base.bringSubviewToFront(view)
-        return self
-    }
-    
-    @discardableResult
-    func layer(_ closure: (_ layer: CALayer) -> Void) -> Self {
-        closure(base.layer)
-        return self
-    }
-    
-    @discardableResult
-    func clipsToBounds(_ clipsToBounds: Bool) -> Self {
-        base.clipsToBounds = clipsToBounds
-        return self
-    }
-    
-    @discardableResult
-    func addGestureRecognizer(_ gestureRecognizer: UIGestureRecognizer) -> Self {
-        base.addGestureRecognizer(gestureRecognizer)
-        return self
-    }
-    
-    @discardableResult
-    func removeGestureRecognizer(_ gestureRecognizer: UIGestureRecognizer) -> Self {
-        base.removeGestureRecognizer(gestureRecognizer)
-        return self
-    }
-    
-    @discardableResult
-    func isUserInteractionEnabled(_ isUserInteractionEnabled: Bool) -> Self {
-        base.isUserInteractionEnabled = isUserInteractionEnabled
-        return self
-    }
-    
-    @discardableResult
-    func addConstraints(_ constraints: [NSLayoutConstraint]) -> Self {
-        base.addConstraints(constraints)
         return self
     }
     
