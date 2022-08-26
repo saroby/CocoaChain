caeda135c51b36de7b9889cfb9adb8d3f3a692a0
Louis Shin <saroby@naver.com>
2022-08-26 12:27:23 +0900

    UILabel updated

--- Sources/CocoaChain/UIKit/UILabel + Chain.swift
+++ Sources/CocoaChain/UIKit/UILabel + Chain.swift
@@ -26,6 +26,9 @@ public extension Chain where T: UILabel {
         return self
     }
     
+    /// this determines the number of lines to draw and what to do when sizeToFit is called. default value is 1 (single line). A value of 0 means no limit
+    /// if the height of the text reaches the # of lines or the height of the view is less than the # of lines allowed, the text will be
+    /// truncated using the line break mode.
     @discardableResult
     func numberOfLines(_ numberOfLines: Int) -> Self {
         base.numberOfLines = numberOfLines
@@ -38,6 +41,29 @@ public extension Chain where T: UILabel {
         return self
     }
     
+    /// default is UIBaselineAdjustmentAlignBaselines
+    @discardableResult
+    func baselineAdjustment(_ baselineAdjustment: UIBaselineAdjustment) -> Self {
+        base.baselineAdjustment = baselineAdjustment
+        return self
+    }
+    
+    /// default is 0.0
+    @discardableResult
+    func minimumScaleFactor(_ minimumScaleFactor: CGFloat) -> Self {
+        base.minimumScaleFactor = minimumScaleFactor
+        return self
+    }
+    
+    /// default is NO
+    func allowsDefaultTighteningForTruncation(_ allowsDefaultTighteningForTruncation: Bool) -> Self {
+        base.allowsDefaultTighteningForTruncation = allowsDefaultTighteningForTruncation
+        return self
+    }
+    
+    /// Specifies the line break strategies that may be used for laying out the text in this label.
+    /// If this property is not set, the default value is NSLineBreakStrategyStandard.
+    /// If the label contains an attributed text with paragraph style(s) that specify a set of line break strategies, the set of strategies in the paragraph style(s) will be used instead of the set of strategies defined by this property.
     @discardableResult
     func lineBreakStrategy(_ lineBreakStrategy: NSParagraphStyle.LineBreakStrategy) -> Self {
         base.lineBreakStrategy = lineBreakStrategy
