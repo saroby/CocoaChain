324d12cdeb3f6d64ea843195b934f8cb666a5ca7
Louis Shin <louis@spoonradio.co>
2022-11-17 11:07:59 +0900

    UIView.layer file added

--- Sources/CocoaChain/UIKit/UIView + Chain.swift
+++ Sources/CocoaChain/UIKit/UIView + Chain.swift
@@ -16,12 +16,6 @@ public extension Chain where T: UIView {
         return self
     }
     
-    @discardableResult
-    func layer(_ closure: (_ layer: CALayer) -> Void) -> Self {
-        closure(base.layer)
-        return self
-    }
-    
     @available(iOS 14.0, *)
     @discardableResult
     func focusGroupIdentifier(_ focusGroupIdentifier: String?) -> Self {
@@ -655,73 +649,6 @@ public extension Chain where T: UIView {
         )
         
         base.bounds = bounds
-        return self
-    }
-    
-    @discardableResult
-    func layer(cornerRadius: CGFloat) -> Self {
-        base.layer.cornerRadius = cornerRadius
-        return self
-    }
-    
-    @available(iOS 13.0, *)
-    @discardableResult
-    func layer(cornerCurve: CALayerCornerCurve) -> Self {
-        base.layer.cornerCurve = cornerCurve
-        return self
-    }
-    
-    @discardableResult
-    func layer(masksToBounds: Bool) -> Self {
-        base.layer.masksToBounds = masksToBounds
-        return self
-    }
-    
-    @discardableResult
-    func layer(borderWidth: CGFloat) -> Self {
-        base.layer.borderWidth = borderWidth
-        return self
-    }
-    
-    @discardableResult
-    func layer(borderColor: CGColor?) -> Self {
-        base.layer.borderColor = borderColor
-        return self
-    }
-    
-    @discardableResult
-    func layer(borderColor: UIColor?) -> Self {
-        base.layer.borderColor = borderColor?.cgColor
-        return self
-    }
-    
-    @discardableResult
-    func layer(shadowColor: UIColor?) -> Self {
-        base.layer.shadowColor = shadowColor?.cgColor
-        return self
-    }
-    
-    @discardableResult
-    func layer(shadowOffset: CGSize) -> Self {
-        base.layer.shadowOffset = shadowOffset
-        return self
-    }
-    
-    @discardableResult
-    func layer(shadowRadius: CGFloat) -> Self {
-        base.layer.shadowRadius = shadowRadius
-        return self
-    }
-    
-    @discardableResult
-    func layer(shadowOpacity: Float) -> Self {
-        base.layer.shadowOpacity = shadowOpacity
-        return self
-    }
-    
-    @discardableResult
-    func layer(shadowPath: CGPath) -> Self {
-        base.layer.shadowPath = shadowPath
         return self
     }
     
