07075bb95ec09aa0a8fe0bfbc0fba36d6e4f954c
Louis Shin <saroby@naver.com>
2023-01-22 22:33:24 +0900

    UIWindow added

---
+++ Sources/CocoaChain/UIKit/UIWindow + Chain.swift
@@ -0,0 +1,66 @@
+import UIKit
+
+public extension Chain where T: UIWindow {
+    
+    @available(iOS 13.0, *)
+    @discardableResult
+    func windowScene(_ windowScene: UIWindowScene?) -> Self {
+        base.windowScene = windowScene
+        return self
+    }
+    
+    @discardableResult
+    func canResizeToFitContent(_ canResizeToFitContent: Bool) -> Self {
+        base.canResizeToFitContent = canResizeToFitContent
+        return self
+    }
+    
+    
+    @discardableResult
+    func screen(_ screen: UIScreen) -> Self {
+        base.screen = screen
+        return self
+    }
+    
+    @discardableResult
+    func windowLevel(_ windowLevel: UIWindow.Level) -> Self {
+        base.windowLevel = windowLevel
+        return self
+    }
+    
+    @discardableResult
+    func becomeKey() -> Self {
+        base.becomeKey()
+        return self
+    }
+    
+    @discardableResult
+    func resignKey() -> Self {
+        base.resignKey()
+        return self
+    }
+    
+    @discardableResult
+    func makeKey() -> Self {
+        base.makeKey()
+        return self
+    }
+    
+    @discardableResult
+    func makeKeyAndVisible() -> Self {
+        base.makeKeyAndVisible()
+        return self
+    }
+    
+    @discardableResult
+    func rootViewController(_ rootViewController: UIViewController?) -> Self {
+        base.rootViewController = rootViewController
+        return self
+    }
+    
+    @discardableResult
+    func sendEvent(_ event: UIEvent) -> Self {
+        base.sendEvent(event)
+        return self
+    }
+}
