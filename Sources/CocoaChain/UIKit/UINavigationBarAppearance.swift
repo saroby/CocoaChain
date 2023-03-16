aed6398ff81950707ee5fe60e0df529bc78dbabc
Louis Shin <saroby@naver.com>
2023-03-16 21:55:30 +0900

    UINavigationBarAppearance added

---
+++ Sources/CocoaChain/UIKit/UINavigationBarAppearance.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+@available(iOS 13.0, *)
+public extension Chain where T: UINavigationBarAppearance {
+    
+    @discardableResult
+    func titleTextAttributes(_ titleTextAttributes: [NSAttributedString.Key : Any]) -> Self {
+        base.titleTextAttributes = titleTextAttributes
+        return self
+    }
+    
+    @discardableResult
+    func titlePositionAdjustment(_ titlePositionAdjustment: UIOffset) -> Self {
+        base.titlePositionAdjustment = titlePositionAdjustment
+        return self
+    }
+    
+    @discardableResult
+    func largeTitleTextAttributes(_ largeTitleTextAttributes: [NSAttributedString.Key : Any]) -> Self {
+        base.largeTitleTextAttributes = largeTitleTextAttributes
+        return self
+    }
+    
+}
